d0dd321f92dfe9de45d427bdac31290921332f61
prannoytank <prannoy23@gmail.com>
2019-11-17 14:20:10 -0800

    unique url visit count per session

--- src/main/scala/com/prannoy/paytmchallenge/Main.scala
+++ src/main/scala/com/prannoy/paytmchallenge/Main.scala
@@ -32,29 +32,34 @@ object Main {
 
     val tmo2: Long = 60 * 60
 
+    println(System.getProperty("user.dir"))
+
+    //sys.exit
     val df = spark
       .read
       .option("delimiter"," ")
       .schema(getSchema())
-      .csv("file:////home/cloudera/projects/WeblogChallenge/data/2015_07_22_mktplace_shop_web_log_sample.log.gz")
+      .csv(s"file:///${System.getProperty("user.dir")}/data/2015_07_22_mktplace_shop_web_log_sample.log.gz")
 
-    //rowsLastWindow(df,spark)
-    //queryBasedWindows(df,spark);
-   timeDifferenceFrameWise(df,spark)
+     timeDifferenceFrameWise(df,spark)
+
+
+    val sessionizedDf = timeDifferenceFrameWise(df,spark)
+
+    //sessionizedDf.show(50,false)
+
+
+    val uniqueUrlVisitPerSesion=sessionizedDf.groupBy($"uid",$"sess_id").agg(countDistinct($"request").as("count"))
+
+    uniqueUrlVisitPerSesion.show(50,false)
+
 
     //plainScalaLogicMethod()
-    //closesetResultSession(df,spark)
 
-    //workingSession(df,spark)
-
-   sys.exit()
+     sys.exit()
 
     val grouped = df.groupBy($"client:port" , window($"timestamp" , "2 minutes")).count
     grouped.select("client:port","window","count").orderBy($"client:port").show(50,false)
-
-
-    val windowSpec = Window.partitionBy("client:port").orderBy("timestamp")
-    val session_window = Window.partitionBy("session").orderBy("timestamp")
   }
 
   def plainScalaLogicMethod(): Unit ={
@@ -98,51 +103,6 @@ object Main {
     }
   }
 
-
-
-  def workingSession(df:DataFrame,spark:SparkSession) = {
-
-    import spark.implicits._
-    def clickSessList(tmo: Long) = udf{ (uid: String, clickList: Seq[Long], tsList: Seq[Long]) =>
-      def sid(n: Long) = s"$uid-$n"
-
-      val sessList = tsList.foldLeft( (List[String](), 0L, 0L) ){ case ((ls, j, k), i) =>
-        if (i == 0) (sid(k + 1) :: ls, 0L, k + 1) else
-        if (j + i < tmo) (sid(k) :: ls, j + i, k) else
-          (sid(k + 1) :: ls, 0L, k + 1)
-      }._1.reverse
-
-      clickList zip sessList
-    }
-
-    val win1 = Window.partitionBy("client:port").orderBy("timestamp")
-
-    val df1 = df.
-      withColumn("ts_diff", unix_timestamp($"timestamp") - unix_timestamp(
-        lag($"timestamp", 1).over(win1))
-      )
-      .withColumn("timestamp_epoch",$"timestamp".cast("long"))
-      .withColumn("time_diff",$"timestamp".cast("long") - lag($"timestamp", 1).over(win1).cast("long"))
-      .withColumn("time_diff_in_a_frame",$"timestamp".cast("long") - first($"timestamp".cast("long"),true).over(win1))
-//      withColumn("ts_diff", when(row_number.over(win1) === 1 || $"ts_diff" >= tmo1, 0L).
-//        otherwise($"ts_diff")
-//      )
-
-    df1.select("client:port","timestamp","ts_diff").orderBy("client:port","timestamp").show(30,false)
-
-    val df2 = df1.
-      groupBy("client:port").agg(
-      collect_list($"timestamp_epoch").as("click_list"), collect_list($"time_diff_in_a_frame").as("ts_list")
-    ).
-      withColumn("click_sess_id",
-        explode(clickSessList(tmo1)($"client:port", $"click_list", $"ts_list"))
-      ).
-      select($"client:port", $"click_sess_id._1".as("click_time"), $"click_sess_id._2".as("sess_id"))
-
-    df2.orderBy($"client:port",$"click_time").show(30,false)
-  }
-
-
   /**
    * closeset result
    * @param df
@@ -185,7 +145,7 @@ object Main {
    * @param df
    * @param spark
    */
-  def timeDifferenceFrameWise(df: DataFrame,spark:SparkSession): Unit ={
+  def timeDifferenceFrameWise(df: DataFrame,spark:SparkSession): DataFrame ={
     import spark.implicits._
 
 
@@ -247,82 +207,31 @@ object Main {
       .withColumn("uid",concat(lit("u"),lit("_"),dense_rank.over(Window.orderBy("client:port"))).cast("string"))
       .withColumn("row_number", row_number() over windowSpec)
       .withColumn("timestamp_epoch",$"timestamp".cast("long"))
-      .withColumn("time_diff",$"timestamp".cast("long") - lag($"timestamp", 1).over(windowSpec).cast("long"))
+      //.withColumn("time_diff",$"timestamp".cast("long") - lag($"timestamp", 1).over(windowSpec).cast("long"))
       .withColumn("time_diff_in_a_frame",$"timestamp".cast("long") - first($"timestamp".cast("long"),true).over(windowSpec))
-        .withColumn("time_diff_frame_minute",$"time_diff_in_a_frame"/60)
+        //.withColumn("time_diff_frame_minute",$"time_diff_in_a_frame"/60)
 
     val df2 = r1.
       groupBy("uid").agg(
-      collect_list($"timestamp_epoch").as("click_list"), collect_list($"time_diff_in_a_frame").as("ts_list")
+      collect_list($"timestamp_epoch").as("timestamp_epoch"), collect_list($"time_diff_in_a_frame").as("ts_list")
     ).
       withColumn("click_sess_id",
-        //explode(clickSessList(tmo1)($"uid", $"click_list", $"ts_list"))
         explode(myFuncWithArg(tmo1)(
           //struct(r1.col("uid"),r1.col("timestamp_epoch"),r1.col("time_diff_in_a_frame")))
-          $"uid", $"click_list", $"ts_list"))
+          $"uid", $"timestamp_epoch", $"ts_list"))
       )
-      //.select($"uid", $"click_sess_id._1".as("click_time"), $"click_sess_id._2".as("sess_id"))
-        .select($"uid", $"click_sess_id.f_timestamp".as("click_time"), $"click_sess_id.f_sid".as("sess_id"))
-    df2.show(50,false)
+        .select($"uid", $"click_sess_id.f_timestamp".as("timestamp_epoch"), $"click_sess_id.f_sid".as("sess_id"))
+    //df2.show(50,false)
 
-    //r1.select("row_number","client:port","uid","timestamp","timestamp_epoch","time_diff","time_diff_in_a_frame","time_diff_frame_minute").show(50,false)
+    val df3 = df2.alias("a").join(r1.alias("b"),Seq("uid","timestamp_epoch"))
+
+
+    //df3.show(50,false)
+
+    df3
   }
 
 
-
-
-
-  /**
-   * row last window
-   * @param df
-   * @param spark
-   */
-  def rowsLastWindow(df: DataFrame,spark:SparkSession) = {
-    import spark.implicits._
-
-    val win1 = Window.partitionBy($"client:port").orderBy($"timestamp")
-    val win2 = Window.partitionBy($"client:port").orderBy($"sessTS")
-
-    val df2 = df.
-      withColumn( "firstTS",
-        when( row_number.over(win1) === 1 || $"timestamp".cast("long") - lag($"timestamp", 1).over(win1).cast("long") >= tmo1 , 0L)
-          .otherwise($"timestamp".cast("long") )
-      ).
-      withColumn( "sessTS",
-        last($"firstTS", ignoreNulls = true).
-          over(win1.rowsBetween(Window.unboundedPreceding, 0))
-      ).
-      withColumn("sessionId", concat(lit("session"), dense_rank.over(win2)))
-
-
-      df2.select("client:port","timestamp","firstTS","sessionId").orderBy("client:port","timestamp","sessionId").show(50,false)
-
-  }
-
-
-
-
-  /**
-   * query based window function
-   * @param dataFrame
-   * @param spark
-   */
-   def queryBasedWindows(dataFrame: DataFrame,spark:SparkSession):Unit = {
-
-     val df3 = dataFrame.withColumnRenamed("client:port","client_port")
-     df3.createOrReplaceTempView("webdf")
-
-     import spark.implicits._
-
-     val s = spark.sql("""SELECT *, CONCAT(client_port,
-      CONCAT('_',SUM(new_session) OVER (PARTITION BY client_port ORDER BY timestamp))
-      ) AS session_id
-      FROM (SELECT * , CASE WHEN UNIX_TIMESTAMP(timestamp) - LAG (UNIX_TIMESTAMP(timestamp))
-      OVER (PARTITION BY client_port ORDER BY timestamp) >= 2 * 60 THEN 1 ELSE 0 END AS new_session FROM webdf ) s1""")
-
-     s.select("client_port","timestamp","session_id").orderBy($"client_port",$"timestamp").show(50,false)
-     sys.exit()
-   }
   /**
    * Schema for web log
    * Referred: https://docs.aws.amazon.com/elasticloadbalancing/latest/classic/access-log-collection.html#access-log-entry-format
